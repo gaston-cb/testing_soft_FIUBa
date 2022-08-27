563428731eb193b214f03e606f5c77dd2fd4a307
gvaldez_ubuntu <gvaldez@iar.unlp.edu.ar>
2022-08-27 16:56:28 -0300

    codigo final para entregar

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Test sensor de temperatura y humedad dht11 para catedra de testing de software de sistemas embebidos 
 
-El test se ha realizado en base al siguiente template para la plataforma raspberry pi pico (RP2040) 
+El test se ha realizado en base al template creado  para la plataforma raspberry pi pico (RP2040). El template puede se encuentra:  https://github.com/gaston-cb/templateRpico. Este template se usa para el proyecto final de la CESE 17 cohorte 
 
 link del datasheet 
 https://www.mouser.com/datasheet/2/758/DHT11-Technical-Data-Sheet-Translated-Version-1143054.pdf
--- app/dht11/inc/dht11.h
+++ app/dht11/inc/dht11.h
@@ -1,3 +1,14 @@
+/**
+ * @file dht11.h
+ * @author Gaston Valdez (gaston.cb.90@gmail.com)
+ * @brief  Biblioteca para leer el sensor dht11 y se comunique con una capa de hardware 
+ *         encargada de leer los bytes del sensor 
+ * @version 0.1
+ * @date 2022-08-27
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -10,11 +21,19 @@ typedef struct {
 
 void read_dht11() ; 
 void init_dht11(uint8_t pin_number_dht_11) ; 
-void get_buffer_rx(uint8_t *buffer) ; 
 /**
  * @brief debe llamarse luego de invocar a read_dht11
  *        Parsea los bytes que reciben desde el hardware   
  * 
+ */
+void get_buffer_rx(uint8_t *buffer) ; 
+
+
+/**
+ * @brief devuelve una estructura dht11_t con los valores que lee desde el hardware o la ultima 
+ *        lectura correcta. 
+ * 
+ * @return dht11_t estructura con los valores correspondientes a temperatura y humedad
  */
 dht11_t read_sensor_data() ; 
 float get_temperature() ; 
